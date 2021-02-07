2217cc6e03aafd307897291feddb72aa2cddf76f
jbooth <slipster216@gmail.com>
2021-02-07 17:56:16 -0500

    adding conditional checks around asset importers

--- Scripts/Editor/ShaderPackageImporter.cs
+++ Scripts/Editor/ShaderPackageImporter.cs
@@ -7,7 +7,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+#if UNITY_2020_1_OR_NEWER
+using UnityEditor.AssetImporters;
+#else
 using UnityEditor.Experimental.AssetImporters;
+#endif
 using System.IO;
 
 namespace JBooth.ShaderPackager
