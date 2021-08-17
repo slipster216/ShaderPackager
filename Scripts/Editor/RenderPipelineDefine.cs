d8bc6cdaa9639e13ae1505c155a1ec0221f57745
jbooth <slipster216@gmail.com>
2021-08-17 13:34:36 -0400

    removing timeout issue in render pipeline defines

--- Scripts/Editor/RenderPipelineDefine.cs
+++ Scripts/Editor/RenderPipelineDefine.cs
@@ -24,96 +24,94 @@ using UnityEditor.PackageManager.Requests;
 namespace JBooth.ShaderPackager
 {
     public static class RenderPipelineDefine
-    {
- 
-        private const string HDRP_PACKAGE = "com.unity.render-pipelines.high-definition";
-        private const string URP_PACKAGE = "com.unity.render-pipelines.universal";
- 
-        private const string TAG_HDRP = "USING_HDRP";
-        private const string TAG_URP = "USING_URP";
- 
+   {
 
- 
- 
- 
- 
-        [UnityEditor.Callbacks.DidReloadScripts]
-        private static void OnScriptsReloaded()
-        {
- 
-            ListRequest packagesRequest = Client.List(true);
- 
-            LoadPackages(packagesRequest);
- 
-        }
-        private static void LoadPackages (ListRequest request)
-        {
- 
-            if (request == null)
-                return;
- 
- 
-            // Wait for request to complete
-            for (int i = 0; i < 1000; i++)
+      private const string HDRP_PACKAGE = "com.unity.render-pipelines.high-definition";
+      private const string URP_PACKAGE = "com.unity.render-pipelines.universal";
+
+      private const string TAG_HDRP = "USING_HDRP";
+      private const string TAG_URP = "USING_URP";
+
+
+
+
+
+      private static ListRequest request;
+      [UnityEditor.Callbacks.DidReloadScripts]
+      private static void OnScriptsReloaded()
+      {
+         request = Client.List(true);
+         EditorApplication.update -= ListProgress;
+         EditorApplication.update += ListProgress;
+      }
+
+      private static void ListProgress()
+      {
+         if (request.IsCompleted)
+         {
+            if (request.Status == StatusCode.Success)
             {
-                if (request.Result != null)
-                    break;
-                Task.Delay(1).Wait();
+               // Find out what packages are installed
+               var packagesList = request.Result.ToList();
+
+               ///Debug.Log("List of offline Unity packages:\n\n" + String.Join("\n", packagesList.Select(x => x.name)) + "\n\n");
+
+               bool hasHDRP = packagesList.Find(x => x.name.Contains(HDRP_PACKAGE)) != null;
+               bool hasURP = packagesList.Find(x => x.name.Contains(URP_PACKAGE)) != null;
+
+
+               DefinePreProcessors(hasHDRP, hasURP);
             }
-            if (request.Result == null)
-                throw new TimeoutException();
- 
-            // Find out what packages are installed
-            var packagesList = request.Result.ToList();
- 
-            ///Debug.Log("List of offline Unity packages:\n\n" + String.Join("\n", packagesList.Select(x => x.name)) + "\n\n");
- 
-            bool hasHDRP = packagesList.Find(x => x.name.Contains(HDRP_PACKAGE)) != null;
-            bool hasURP = packagesList.Find(x => x.name.Contains(URP_PACKAGE)) != null;
- 
- 
-            DefinePreProcessors(hasHDRP, hasURP);
- 
-        }
- 
- 
- 
-        private static void DefinePreProcessors(bool defineHDRP, bool defineURP)
-        {
- 
-            string originalDefineSymbols;
-            string newDefineSymbols;
- 
-            List<string> defined;
-            BuildTargetGroup platform = EditorUserBuildSettings.selectedBuildTargetGroup;
- 
-            string log = string.Empty;
- 
-            // foreach(var platform in avaliablePlatforms)
-            originalDefineSymbols = PlayerSettings.GetScriptingDefineSymbolsForGroup(platform);
-            defined = originalDefineSymbols.Split(';').Where(x => !String.IsNullOrWhiteSpace(x)).ToList();
- 
- 
-            Action<bool, string> AppendRemoveTag = (stat, tag) =>
+            else
             {
-                if (stat && !defined.Contains(tag))
-                    defined.Add(tag);
-                else if (!stat && defined.Contains(tag))
-                    defined.Remove(tag);
-            };
- 
-            AppendRemoveTag(defineHDRP, TAG_HDRP);
-            AppendRemoveTag(defineURP, TAG_URP);
- 
-            newDefineSymbols = string.Join(";", defined);
-            if(originalDefineSymbols != newDefineSymbols)
-            {
-                PlayerSettings.SetScriptingDefineSymbolsForGroup(platform, newDefineSymbols);
+               Debug.Log(request.Error.message);
             }
- 
-        }
- 
-    }
+
+            EditorApplication.update -= ListProgress;
+         }
+      }
+
+
+      private static void DefinePreProcessors(bool defineHDRP, bool defineURP)
+      {
+
+         string originalDefineSymbols;
+         string newDefineSymbols;
+
+         List<string> defined;
+         //List<BuildTargetGroup> avaliablePlatforms = Enum.GetValues(typeof(BuildTargetGroup)).Cast<BuildTargetGroup>().ToList();
+         BuildTargetGroup platform = EditorUserBuildSettings.selectedBuildTargetGroup;
+
+         string log = string.Empty;
+
+         // foreach(var platform in avaliablePlatforms)
+         originalDefineSymbols = PlayerSettings.GetScriptingDefineSymbolsForGroup(platform);
+         defined = originalDefineSymbols.Split(';').Where(x => !String.IsNullOrWhiteSpace(x)).ToList();
+
+
+         Action<bool, string> AppendRemoveTag = (stat, tag) =>
+         {
+            if (stat && !defined.Contains(tag))
+               defined.Add(tag);
+            else if (!stat && defined.Contains(tag))
+               defined.Remove(tag);
+         };
+
+         AppendRemoveTag(defineHDRP, TAG_HDRP);
+         AppendRemoveTag(defineURP, TAG_URP);
+
+         newDefineSymbols = string.Join(";", defined);
+         if (originalDefineSymbols != newDefineSymbols)
+         {
+            PlayerSettings.SetScriptingDefineSymbolsForGroup(platform, newDefineSymbols);
+         }
+
+      }
+
+
+
+
+   }
 }
 
 #endif
